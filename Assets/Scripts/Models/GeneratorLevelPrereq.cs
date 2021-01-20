632170797a9a73fe1cfc492bd0a2a9a7d8c8a5ea
Lucio Aquino <luquio@icloud.com>
2021-01-20 16:01:13 -0300

    Added Soft prereq when specific generator reaches level

---
+++ Assets/Scripts/Models/GeneratorLevelPrereq.cs
@@ -0,0 +1,26 @@
+﻿using BigfootSdk;
+using BigfootSdk.Core.Idle;
+
+public class GeneratorLevelPrereq : IPrerequisite
+{
+    /// <summary>
+    /// The generator name.
+    /// </summary>
+    public string Name;
+
+    /// <summary>
+    /// The min level.
+    /// </summary>
+    public int MinLevel;
+		
+    /// <summary>
+    /// Checks if the prerequisite is valid
+    /// </summary>
+    public override bool Check ()
+    {
+        // Grab the generator level
+        int generatorAmount = GeneratorManager.Instance.GetGeneratorAmount(Name);
+
+        return generatorAmount >= MinLevel;
+    }
+}
